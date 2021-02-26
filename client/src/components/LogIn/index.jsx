24dd64bdff14273b8dc692c2feaf1bd10645206d
Khang <khang.job@gmail.com>
2021-02-26 21:09:50 +0700

    Login style

--- client/src/components/LogIn/index.jsx
+++ client/src/components/LogIn/index.jsx
@@ -1,10 +1,23 @@
 import './LogIn.scss'
 
 function LogIn() {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    let formData = new FormData(e.target)
+    console.log(Object.fromEntries(formData.entries()))
+  }
+
   return (
     <div className="LogInPage">
       <div className="LogInPage__wrapper">
-        test
+        <div className="LogInPage__title">Login Form</div>
+        <form className="LogInPage__form" onSubmit={handleSubmit}>
+          <input placeholder="Enter Email" type="email" name="emal" id="email" required />
+          <input placeholder="Enter Password" type="password" name="password" id="password" required />
+          <div className="LogInPage__submit">
+            <button type="submit">Submit</button>
+          </div>
+        </form>
       </div>
     </div>
   )
